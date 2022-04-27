063d172f04611364881d577e26094aecc8599458
ivs <kopriva.rad@gmail.com>
2022-04-27 23:52:35 +0200

    :zap:

--- proj2.c
+++ proj2.c
@@ -7,7 +7,8 @@
 #include <sys/wait.h>
 #include <time.h>
 
-typedef struct{
+typedef struct
+{
     int num;
     int count;
     sem_t mutex;
@@ -15,8 +16,8 @@ typedef struct{
     sem_t turnstile2;
 } Barrier;
 
-
-Barrier create_barrier(int n){
+Barrier create_barrier(int n)
+{
     Barrier *barrier = malloc(sizeof(Barrier));
     barrier->num = n;
     barrier->count = 0;
@@ -25,41 +26,13 @@ Barrier create_barrier(int n){
     sem_init(&barrier->turnstile2, 1, 0);
     return *barrier;
 }
-   /* def __init__ ( self , n ):
-    self .n = n
-    self . count = 0
-    self . mutex = Semaphore (1)
-    self . turnstile = Semaphore (0)
-    self . turnstile2 = Semaphore (0)
 
-
-    def phase1 ( self ):
-    self . mutex . wait ()
-    self . count += 1
-
-    if self . count == self .n:
-    self . turnstile . signal ( self . n)
-    self . mutex . signal ()
-    self . turnstile . wait ()
-
-    def phase2 ( self ):
-    self . mutex . wait ()
-    self . count -= 1
-    if self . count == 0:
-    self . turnstile2 . signal ( self .n)
-    self . mutex . signal ()
-    self . turnstile2 . wait ()
-
-    def wait ( self ):
-    self . phase1 ()
-    self . phase2 ()*/
-
-
-
-void barrier_wait(Barrier *barrier){
+void barrier_wait(Barrier *barrier)
+{
     sem_wait(&barrier->mutex);
     barrier->count++;
-    if(barrier->count == barrier->num){
+    if (barrier->count == barrier->num)
+    {
         for (int i = 0; i < barrier->num; i++)
         {
             sem_post(&barrier->turnstile1);
@@ -67,34 +40,39 @@ void barrier_wait(Barrier *barrier){
     }
     sem_post(&barrier->mutex);
     sem_wait(&barrier->turnstile1);
-    
+
     sem_wait(&barrier->mutex);
     barrier->count--;
-    if(barrier->count == 0){
+    if (barrier->count == 0)
+    {
         for (int i = 0; i < barrier->num; i++)
         {
             sem_post(&barrier->turnstile2);
         }
-        
     }
     sem_post(&barrier->mutex);
     sem_wait(&barrier->turnstile2);
 }
 
+void barrier_destroy(Barrier *barrier)
+{
+    sem_destroy(&barrier->mutex);
+    sem_destroy(&barrier->turnstile1);
+    sem_destroy(&barrier->turnstile2);
+}
 
 typedef struct shared_mem
 {
     int count;
-    int oxy_count;
-    int hydro_count;
+    int molecule_count;
+    int remaining_oxygen;
+    int remaining_hydrogen;
     sem_t semtex;
     sem_t mutex;
-    sem_t oxygen_sem;
-    sem_t hydrogen_sem;
     sem_t hydrogen_queue;
     sem_t oxygen_queue;
     Barrier barrier;
-}shared_mem_t;
+} shared_mem_t;
 
 typedef struct argum
 {
@@ -102,33 +80,34 @@ typedef struct argum
     int tb;
     int no;
     int nh;
-}argum_t;
+} argum_t;
 
 argum_t *argum;
 shared_mem_t *mem;
 FILE *fp;
 
-void create_sh_memory(){
+void create_sh_memory()
+{
     mem = (shared_mem_t *)mmap(NULL, sizeof(shared_mem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
     mem->count = 1;
-    mem->oxy_count = 1;
-    mem->hydro_count = 1;
-    sem_init(&mem->oxygen_sem, 1, 1);
-    sem_init(&mem->hydrogen_sem, 1, 1);
+    mem->molecule_count = 1;
+    mem->remaining_oxygen = argum->no;
+    mem->remaining_hydrogen = argum->nh;
     sem_init(&mem->mutex, 1, 1);
     sem_init(&mem->hydrogen_queue, 1, 2);
     sem_init(&mem->oxygen_queue, 1, 1);
     mem->barrier = create_barrier(3);
 }
 
-void set_arguments(char *argv[]){
+void set_arguments(char *argv[])
+{
     argum = (argum_t *)malloc(sizeof(argum_t));
     argum->ti = atoi(argv[3]);
     argum->tb = atoi(argv[4]);
     argum->no = atoi(argv[1]);
     argum->nh = atoi(argv[2]);
 }
-//checks arguments
+// checks arguments
 void check_arg(int argc, char *argv[])
 {
     if (argc != 5)
@@ -148,13 +127,13 @@ void check_arg(int argc, char *argv[])
         exit(1);
     }
 
-    if (0>atoi(argv[3]) || atoi(argv[3])>1000)
+    if (0 > atoi(argv[3]) || atoi(argv[3]) > 1000)
     {
         fprintf(stderr, "TI cant be less than zero or greater than 1000\n");
         exit(1);
     }
 
-    if (0>atoi(argv[4]) || atoi(argv[4])>1000)
+    if (0 > atoi(argv[4]) || atoi(argv[4]) > 1000)
     {
         fprintf(stderr, "TB cant be less than zero or greater than 1000\n");
         exit(1);
@@ -162,93 +141,147 @@ void check_arg(int argc, char *argv[])
     set_arguments(argv);
 }
 
+void free_non_shared()
+{
+    free(argum);
+    fclose(fp);
+}
 
-void write_into_file(char *mol, int id,char *message){
-    fprintf(fp, "%d: %s %d: %s\n", mem->count++,mol, id, message);
+// free shared memory
+
+void free_shared()
+{
+    sem_destroy(&mem->mutex);
+    sem_destroy(&mem->hydrogen_queue);
+    sem_destroy(&mem->oxygen_queue);
+    barrier_destroy(&mem->barrier);
+    munmap(mem, sizeof(shared_mem_t));
+}
+
+void write_into_file(char atom, int id, char *message)
+{
+    fprintf(fp, "%d: %c %d: %s\n", mem->count++, atom, id, message);
     fflush(fp);
 }
 
-void create_oxygen(int ti)
+void oxygen_check(int idO)
+{
+    if (mem->remaining_hydrogen <= 1)
+    {
+        write_into_file('O', idO, "not enough H");
+        mem->remaining_oxygen--;
+        sem_post(&mem->mutex);
+        sem_post(&mem->oxygen_queue);
+        free_non_shared();
+        _exit(0);
+    }
+}
+
+void hydrogen_check(int idH)
+{
+    if (mem->remaining_oxygen < 1 || mem->remaining_hydrogen <= 1)
+    {
+        write_into_file('H', idH, "not enough O or H");
+        mem->remaining_hydrogen--;
+        sem_post(&mem->mutex);
+        sem_post(&mem->hydrogen_queue);
+        free_non_shared();
+        _exit(0);
+    }
+}
+
+void start_atom(char atom, int id)
+{
+    srand(time(NULL) ^ getpid());
+    sem_wait(&mem->mutex);
+    write_into_file(atom, id, "started");
+    sem_post(&mem->mutex);
+    usleep(rand() % argum->ti * 1000);
+}
+
+void create_molecule(char atom, int id)
+{
+    sem_wait(&mem->mutex);
+    sem_post(&mem->mutex);
+    char buffer[100];
+    sprintf(buffer, "creating molecule %d", mem->molecule_count);
+    write_into_file(atom, id, buffer);
+    if (atom == 'O')
+    {
+        usleep(rand() % argum->tb * 1000);
+    }
+
+    barrier_wait(&mem->barrier);
+    sprintf(buffer, "molecule %d created", mem->molecule_count);
+    write_into_file(atom, id, buffer);
+}
+
+void oxygen_queue(int idO){
+    sem_wait(&mem->oxygen_queue);
+    sem_wait(&mem->mutex);
+    oxygen_check(idO);
+    sem_post(&mem->mutex);
+    barrier_wait(&mem->barrier);
+    mem->remaining_oxygen--;
+    create_molecule('O', idO);
+    barrier_wait(&mem->barrier);
+    sem_wait(&mem->mutex);
+    mem->molecule_count++;
+    sem_post(&mem->mutex);
+    sem_post(&mem->oxygen_queue);
+}
+
+void hydrogen_queue(int idH){
+    sem_wait(&mem->hydrogen_queue);
+    sem_wait(&mem->mutex);
+    hydrogen_check(idH);
+    sem_post(&mem->mutex);
+
+    barrier_wait(&mem->barrier);
+    mem->remaining_hydrogen--;
+    create_molecule('H', idH);
+
+    barrier_wait(&mem->barrier);
+    sem_post(&mem->hydrogen_queue);
+}
+
+void create_oxygen(int idO)
 {
     pid_t pid = fork();
 
     if (pid == 0)
     {
-            srand(time(NULL)^getpid());
+        start_atom('O', idO);
 
-            sem_wait(&mem->mutex);
-            int idO = mem->oxy_count;
-            write_into_file("O", idO, "started");
-            mem->oxy_count++;
-            sem_post(&mem->mutex);
+        sem_wait(&mem->mutex);
+        write_into_file('O', idO, "going to queue");
+        sem_post(&mem->mutex);
 
-            usleep(rand() % ti * 1000);
-
-            sem_wait(&mem->mutex);
-            write_into_file("O", idO, "going to queue");
-
-            sem_post(&mem->mutex);
+        oxygen_queue(idO);
 
 
-
-
-
-            sem_wait(&mem->oxygen_queue);
-
-            barrier_wait(&mem->barrier);
-
-
-
-            sem_post(&mem->oxygen_queue);
-
-
-
-
+        free_non_shared();
         _exit(0);
     }
 }
 
-void create_hydrogen(int ti)
+void create_hydrogen(int idH)
 {
     pid_t pid = fork();
 
     if (pid == 0)
     {
-            srand(time(NULL)^getpid());
-            
-            sem_wait(&mem->mutex);
-            int idH = mem->hydro_count;
-            write_into_file("H", idH, "started");
-            mem->hydro_count++;
-            sem_post(&mem->mutex);
+        srand(time(NULL) ^ getpid());
 
-            usleep(rand() % ti * 1000);
-            sem_wait(&mem->mutex);
-            write_into_file("H", idH, "going to queue");
+        start_atom('H', idH);
+        sem_wait(&mem->mutex);
+        write_into_file('H', idH, "going to queue");
 
-            sem_post(&mem->mutex);
-
-
-
-            sem_wait(&mem->hydrogen_queue);
-            
-
-            barrier_wait(&mem->barrier);
-
-
-            sem_post(&mem->hydrogen_queue);
-
-
-
-
+        sem_post(&mem->mutex);
+        hydrogen_queue(idH);
+        free_non_shared();
         _exit(0);
     }
-}
-
-//free shared memory
-void sh_free()
-{
-    munmap(mem, sizeof(shared_mem_t));
 }
 
 void open_file()
@@ -268,22 +301,21 @@ int main(int argc, char *argv[])
     create_sh_memory();
     open_file();
 
-    for (int i = 0; i < argum->no; i++)
+    for (int i = 1; i < argum->no + 1; i++)
     {
-        create_oxygen(argum->ti);
+        create_oxygen(i);
     }
 
-    for (int i = 0; i < argum->nh; i++)
+    for (int i = 1; i < argum->nh + 1; i++)
     {
-        create_hydrogen(argum->ti);
+        create_hydrogen(i);
     }
 
     for (int i = 0; i < argum->nh + argum->no; i++)
     {
         waitpid(-1, NULL, 0);
     }
-
-    fclose(fp);
-    sh_free();
+    free_non_shared();
+    free_shared();
     exit(0);
 }
